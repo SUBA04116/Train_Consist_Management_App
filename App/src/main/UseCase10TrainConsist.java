c97985b7cf2b8f648e4681e900fb323669b225fc
Inbasekaran12 <in5904@srmist.edu.in>
2026-04-18 14:56:07 +0530

    feat: implement total seating capacity calculation using stream reduce

---
+++ App/src/main/UseCase10TrainConsist.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class UseCase10TrainConsist {
+
+    // Bogie model
+    static class Bogie {
+        String name;
+        int capacity;
+
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+    }
+
+    // Method for aggregation (USED IN TEST)
+    public static int calculateTotalCapacity(List<Bogie> bogies) {
+        return bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("===== UC10 Count Total Seats in Train =====\n");
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+
+        System.out.println("Bogies in Train:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+        int total = calculateTotalCapacity(bogies);
+
+        System.out.println("\nTotal Seating Capacity of Train: " + total);
+        System.out.println("UC10 aggregation completed...");
+    }
+}
